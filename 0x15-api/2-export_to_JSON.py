72fcd7a1930dbcc80d5ea9dda38b2086cb8fa2ab
francotaind <francotaind00@gmail.com>
2024-05-06 20:46:27 +0300

    devops_configuration

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,44 +1,19 @@
 #!/usr/bin/python3
-"""
-A Script that, uses a REST API, for a given employee ID, returns
-information about his/her TODO list progress
-and exports data in the JSON format.
-"""
-
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
-from sys import argv
-
+import sys
 
 if __name__ == "__main__":
-    session_request = requests.Session()
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    employee_id = argv[1]
-    todo_url = ('https://jsonplaceholder.typicode.com/users/{}/'
-                'todos').format(employee_id)
-    name_url = ('https://jsonplaceholder.typicode.com/users/{}'
-                .format(employee_id))
-
-    todo_response = session_request.get(todo_url)
-    name_response = session_request.get(name_url)
-
-    todo_data = todo_response.json()
-    employee_username = name_response.json()['username']
-
-    tasks = []
-
-    for task in todo_data:
-        tasks.append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": employee_username
-        })
-
-    user_data = {
-        employee_id: tasks
-    }
-
-    file_json = '{}.json'.format(employee_id)
-
-    with open(file_json, "w") as jsonfile:
-        json.dump(user_data, jsonfile, indent=4)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
